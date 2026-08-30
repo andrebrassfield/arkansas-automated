7001ebe4ab01f19dab7ff8be495cf5f2648ba0ab
andrebrassfield <andrebrassfield@gmail.com>
2026-08-30 16:16:00 -0500

    feat: point workshop CTAs at Arkansas Automated Whop checkout
    Seat purchase is $497 on Whop; fit-assessment stays the in-page form.

---
+++ src/lib/booking.ts
@@ -0,0 +1,6 @@
+/** Public workshop checkout. Not localhost. Not AA Book. */
+export const WORKSHOP_WHOP_URL =
+  "https://whop.com/arkansasautomated/front-desk-workshop-f9/";
+
+export const WORKSHOP_CAL_URL =
+  "https://cal.com/andre-brassfield-97stra/front-desk-workshop";
