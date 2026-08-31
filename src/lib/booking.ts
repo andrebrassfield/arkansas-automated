c99ea02b550dde4445a65503fa9d94cefd80ce21
andrebrassfield <andrebrassfield@gmail.com>
2026-08-30 20:24:22 -0500

    feat: point workshop CTAs at live AA Book checkout

--- src/lib/booking.ts
+++ src/lib/booking.ts
@@ -1,6 +1,3 @@
-/** Public workshop checkout. Not localhost. Not AA Book. */
-export const WORKSHOP_WHOP_URL =
-  "https://whop.com/arkansasautomated/front-desk-workshop-f9/";
-
-export const WORKSHOP_CAL_URL =
-  "https://cal.com/andre-brassfield-97stra/front-desk-workshop";
+/** Public workshop checkout on AA Book. */
+export const WORKSHOP_BOOK_URL =
+  "https://book.arkansasautomated.com/book/front-desk-workshop";
